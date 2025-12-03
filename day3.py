f4e8fbcef9d0f5bb7c7997a9d239d2fb81a27e10
Gergely Toth <gergelytothdev@gmail.com>
2025-12-03 18:15:45 +0100

    Day3:Part2

--- day3.py
+++ day3.py
@@ -7,24 +7,27 @@ TEST = """987654321111111
 818181911112111"""
 
 def solve(lines):
+    digit_count = 12
     result = 0
 
     # we keep track of the max number and we find the highest after that
     # special handling for the last number in the line
+    # we keep the same if/elif structure with an array
     for line in lines:
-        first_digit = 0
-        second_digit = 0
+        digits = [0] * digit_count
         
         for i in range(len(line)):
             n = int(line[i])
 
-            if n > first_digit and i != len(line)-1:
-                first_digit = n
-                second_digit = 0
-            elif n > second_digit:
-                second_digit = n
-
-        result += int(str(first_digit) + str(second_digit))
+            for d in range(len(digits)):
+                # line is 7 long (0-6), digit count is 3 (4,5,6), d is 1, i must be <= 5
+                if n > digits[d] and i <= len(line) - digit_count + d:
+                    digits[d] = n
+                    #anull everything after it
+                    digits[d+1:] = [0] * len(digits[d+1:])
+                    break
+    
+        result += int("".join([str(d) for d in digits]))
     
     return result
 
