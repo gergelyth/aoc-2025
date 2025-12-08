8bcebe29cd4754811b7657855221cdf716b74af9
Gergely Toth <gergelytothdev@gmail.com>
2025-12-08 18:35:17 +0100

    Day8:Part2

--- day8.py
+++ day8.py
@@ -1,7 +1,6 @@
 import sys
 import math
 from util import get_lines
-from collections import Counter
 
 TEST = """162,817,812
 57,618,57
@@ -25,8 +24,6 @@ TEST = """162,817,812
 425,690,689"""
 
 def solve(lines):
-    result = 0
-
     coordinates = [tuple(map(int, line.split(","))) for line in lines]
     distances = {}
     for i in range(len(coordinates)):
@@ -35,30 +32,31 @@ def solve(lines):
 
     sorted_dist = sorted(distances.items(), key=lambda x: x[1])
 
+    circuits = {}
     point_to_circuit = {}
     new_circuit_id = 0
-    connections_made = 0
     for (coord1, coord2), _ in sorted_dist:
         circuit1, circuit2 = point_to_circuit.get(coord1), point_to_circuit.get(coord2)
         if circuit1 is None and circuit2 is None:
             point_to_circuit[coord1] = point_to_circuit[coord2] = new_circuit_id
+            circuits[new_circuit_id] = set([coord1, coord2])
             new_circuit_id += 1
         elif circuit1 is None or circuit2 is None:
-            point_to_circuit[coord1] = point_to_circuit[coord2] = circuit1 if circuit1 is not None else circuit2
+            new_circuit = circuit1 if circuit1 is not None else circuit2
+            point_to_circuit[coord1] = point_to_circuit[coord2] = new_circuit
+            circuits[new_circuit].update([coord1, coord2])
         else:
             #both have circuits
             if circuit1 != circuit2:
-                #we need to merge them (circuit2 to circuit1) - this is inefficient for now, we could improve this
-                for point, circuit in point_to_circuit.items():
-                    if circuit == circuit2:
-                        point_to_circuit[point] = circuit1
+                for merging_point in circuits[circuit2]:
+                    circuits[circuit1].add(merging_point)
+                    point_to_circuit[merging_point] = circuit1
+                del circuits[circuit2]
 
-        connections_made += 1
-        if connections_made == 1000:
+        if len(circuits) == 1 and len(point_to_circuit) == len(coordinates):
             break
 
-    top_3 = Counter(point_to_circuit.values()).most_common(3)
-    return math.prod([x[1] for x in top_3])
+    return coord1[0] * coord2[0]
 
 def read_input():
     if not sys.stdin.isatty():
