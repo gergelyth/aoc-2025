1c5ad8e20d2f993906b9f86769abdeb02fc8962f
Gergely Toth <gergelytothdev@gmail.com>
2025-12-10 18:45:58 +0100

    Day10: Part2 - promising, but too high result

--- day10.py
+++ day10.py
@@ -1,49 +1,27 @@
 import sys
+import time
 from util import get_lines
 
 TEST = """[.##.] (3) (1,3) (2) (2,3) (0,2) (0,1) {3,5,4,7}
 [...#.] (0,2,3,4) (2,3) (0,4) (0,1,2) (1,2,3,4) {7,5,12,7,2}
 [.###.#] (0,1,2,3,4) (0,3,4) (0,1,2,4,5) (1,2) {10,11,11,5,10,5}"""
 
-def distribute(total, slots):
-    if slots == 1:
-        yield [total]
-        return
-    for first in range(total, -1, -1):
-        for rest in distribute(total - first, slots - 1):
-            yield [first] + rest
-
-def calc(end_state, button_configs, joltages, i):
-    if joltages[i] == 0:
-        return calc(end_state, button_configs, joltages, i+1)
-
-    needed_presses = joltages[i]
-    relevant_buttons = {key: value for key, value in button_configs.items() if i in value}
-    if len(relevant_buttons) == 0:
+def calc(target, buttons, i):
+    button = buttons[i]    
+    if i == len(buttons)-1:
+        if target % button == 0:
+            return target // button
         return -1
+
+    max_presses = target // button
+    if max_presses == 0:
+        return calc(target, buttons, i+1)
     
-    remaining_buttons = {key: value for key, value in button_configs.items() if key not in relevant_buttons}
-    for distribution in distribute(needed_presses, len(relevant_buttons)):
-        new_target = end_state - sum(distribution[index] * key for index, key in enumerate(relevant_buttons))
-        if new_target < 0:
-            continue
-
-        new_joltage = list(joltages)
-        for index, key in enumerate(relevant_buttons):
-            # new_joltage -= button_press_count * button_config
-            for x in relevant_buttons[key]:
-                new_joltage[x] -= distribution[index]
-
-        if any([x < 0 for x in new_joltage]):
-            continue
-
-        if all([x == 0 for x in new_joltage]):
-            return needed_presses
-
-        rec = calc(new_target, remaining_buttons, new_joltage, i+1)
+    for j in range(max_presses, -1, -1):
+        rec = calc(target - j * button, buttons, i+1)
         if rec == -1:
             continue
-        return needed_presses + rec
+        return j + rec
 
     return -1
 
@@ -53,28 +31,37 @@ def calc(end_state, button_configs, joltages, i):
 def solve(lines):
     result = 0
 
-    for line in lines:
+    start = time.time()
+    for l_idx, line in enumerate(lines):
         parts = line.split()
         #TODO can we just simply parse a tuple in python?
         buttons_raw, joltages = [list(map(int, button[1:-1].split(","))) for button in parts[1:-1]], [int(n) for n in parts[-1][1:-1].split(",")]
 
-        end_state = 0
+        target = 0
+        carry = 0
         for i, joltage in enumerate(reversed(joltages)):
-            end_state += 2 ** i * joltage
-        
+            if i == 0:
+                target += ((joltage+carry) % 10)
+            else:
+                target += ((joltage+carry) % 10) * 10 ** i
+            carry = (joltage+carry) // 10
+
+        if carry > 0:
+            target += carry * 10 ** (i+1)
+
         buttons_arr = [["1" if i in button_raw else "0" for i in range(len(joltages))] for button_raw in buttons_raw]
-        buttons = [int("".join(b), 2) for b in buttons_arr]
-        button_configs = dict(reversed(sorted(zip(buttons, buttons_raw))))
+        buttons = [int("".join(b)) for b in buttons_arr]
 
-        print(joltages)
-        button_configs = dict(sorted(button_configs.items(), key=lambda item: sum(joltages[i] for i in item[1]), reverse=True))
-        print(button_configs)
-        # print(buttons[0] * 1 + buttons[1] * 3 + buttons[3] * 3 + buttons[4] * 1 + buttons[5] * 2)
-        # print(end_state)
+        buttons.sort(reverse=True)
+        calc_result = calc(target, buttons, 0)
+        if calc_result == -1:
+            raise Exception(f"No result for {line}")
+        result += calc_result
 
-        result += calc(end_state, button_configs, joltages, 0)
-        print("Line done")
+        print(f"Line done {l_idx+1}/{len(lines)}")
 
+    elapsed = time.time() - start
+    print(f"Elapsed: {elapsed:.2f} seconds")
     return result
 
 def read_input():
