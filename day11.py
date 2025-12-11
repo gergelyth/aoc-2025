57859ca940cb51873d3df6e49431949bcccd3ef2
Gergely Toth <gergelytothdev@gmail.com>
2025-12-11 18:46:58 +0100

    Day11:Part2

--- day11.py
+++ day11.py
@@ -1,27 +1,52 @@
 import sys
 from util import get_lines
 from collections import deque, defaultdict
+from dataclasses import dataclass
 
-TEST = """aaa: you hhh
-you: bbb ccc
-bbb: ddd eee
-ccc: ddd eee fff
-ddd: ggg
-eee: out
-fff: out
+TEST = """svr: aaa bbb
+aaa: fft
+fft: ccc
+bbb: tty
+tty: ccc
+ccc: ddd eee
+ddd: hub
+hub: fff
+eee: dac
+dac: fff
+fff: ggg hhh
 ggg: out
-hhh: ccc fff iii
-iii: out"""
+hhh: out"""
+
+@dataclass
+class Paths:
+    all_paths: int
+    with_dac: int
+    with_fft: int
+    final_paths: int
 
 def find_path(node, graph, cache):
     if node == "out":
-        return 1
+        return Paths(1,0,0,0)
     if node in cache:
         return cache[node]
     
-    result = 0
+    result = Paths(0,0,0,0)
     for edge in graph[node]:
-        result += find_path(edge, graph, cache)
+        new_path = find_path(edge, graph, cache)
+        result.all_paths += new_path.all_paths
+        if node == "fft":
+            result.final_paths += new_path.with_dac
+            #all of these are converted to final paths
+            result.with_dac += 0
+            result.with_fft += new_path.all_paths - new_path.with_fft
+        elif node == "dac":
+            result.final_paths += new_path.with_fft
+            result.with_fft += 0
+            result.with_dac += new_path.all_paths - new_path.with_dac
+        else:
+            result.final_paths += new_path.final_paths
+            result.with_dac += new_path.with_dac
+            result.with_fft += new_path.with_fft
 
     cache[node] = result
     return result
@@ -30,7 +55,7 @@ def solve(lines):
     graph = {(p := line.split(":"))[0]: p[1].split() for line in lines}
     cache = {}
 
-    return find_path("you", graph, cache)
+    return find_path("svr", graph, cache)
 
 def read_input():
     if not sys.stdin.isatty():
